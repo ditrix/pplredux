7fe3837ac1646a5d29d6dcb2ebb1da5cf9733d16
ditrix <ditrix2005@gmail.com>
2019-08-15 12:01:44 +0300

    refactoring hatml css

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export const Footer = () => {
+	return(
+		<>
+			<a href={'mailto:dmvoloshin@gmail.com'}>dmvoloshin@gmail.com</a>&nbsp;
+      <a href={'https://randomuser.me/'} target={'_blank'}>Datasource: Random User Generator</a>
+		</>
+	)
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,8 +3,9 @@ import {connect} from 'react-redux'
 import {actionGetPeople,actionClearPeople,  actionLoadPeople} from '../action/PeopleActions'
 import  {actionSignIn, actionSignOut} from '../action/UserActions'
 import {data} from '../devdata/data'
+import {Footer} from '../components/Footer'
 import Peoples from '../components/Peoples'
-import AskoHeader from '../AskoHeader'
+//import AskoHeader from '../AskoHeader'
 
 import {API_CLIENT_ID} from '../constants/apiId' 
 import logo from '../logo.svg';
@@ -47,27 +48,39 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-           <AskoHeader />
-           <div className="user-block">
-             <h3>{userName}</h3>
-             <img src={userPic} alt="user" />
-             <button  className="button-login" onClick={this.butttonAuthClick.bind(this)} value="login">
-              {(isLoged)?<span>Logout</span>:<span>Login</span>}
-             </button>               
-           </div>
-           
+           {/*<AskoHeader />*/}
+         <nav>
+         
+            <ul>
+              <li><button onClick={()=>{this.props.loadData()}}>Load</button></li>
+              <li><button onClick={this.butttonGetDataClicked.bind(this)}>Open</button></li>
+              <li><button onClick={()=>{this.props.clearPeople()} }>Clear</button></li>
+              <li>
+                <div className="user-block">
+                    <h3>{userName}</h3>
+                   <img src={userPic} alt="user" />
+                   <button  className="button-login" onClick={this.butttonAuthClick.bind(this)} value="login">
+                    {(isLoged)?<span>Logout</span>:<span>Login</span>}
+                   </button>               
+
+                 </div>
+                
+              </li>
+            </ul>
+
+
+            
+
+        </nav>           
         </header>
-        <nav>
-          
-            <button onClick={()=>{this.props.loadData()}}>Load</button>
-            <button onClick={this.butttonGetDataClicked.bind(this)}>Open</button>
-            <button onClick={()=>{this.props.clearPeople()} }>Clear</button>
-        </nav>
+   
         <main>
         { !this.props.people.isLoaded ? <p>loading...</p>:<Peoples people={people} /> }
 
       </main>
-        <footer>footer</footer>
+      <footer>
+        <Footer />
+      </footer>
       </div>
     );
   }
